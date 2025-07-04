25d327c3f0b4896268e9665a2db70aab0d33fc0c
Bala Pavan Kumar Siripepalli <141495715+BalaPavan19@users.noreply.github.com>
2025-07-04 19:44:22 +0530

    Update Basemodel.py

--- Basemodel.py
+++ Basemodel.py
@@ -1,14 +1,1 @@
-from sqlalchemy.orm import as_declarative
-from sqlalchemy import Column,Integer,String
-
-@as_declarative()
-class Base():
-    pass
-
-class Users(Base):
-    __tablename__ = "User"
-    id = Column(Integer,primary_key=True)
-    name = Column(String,nullable=False)
-
-print(Users.__tablename__)
-print(type(Users.id))
+# Api - Run through Main.py - Apis to each 
